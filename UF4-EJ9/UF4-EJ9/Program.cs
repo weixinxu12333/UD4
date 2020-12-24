0232fcdb3414296e727da5b01caaf25ead02c9f0
Weixin <weixin.xu.2018@gmail.com>
2020-12-24 21:39:31 +0100

    EX9

---
+++ UF4-EJ9/UF4-EJ9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UF4_EJ9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mostrando numeros pares de 1 a 100 con for");
+            for (int i = 1; i < 101; i++)
+            {
+                if (i%2==0) { 
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}

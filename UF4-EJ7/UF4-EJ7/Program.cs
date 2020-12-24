888cddae154020cb4606425d82d509dedb1dc7fc
Weixin <weixin.xu.2018@gmail.com>
2020-12-24 21:38:32 +0100

    EX7

---
+++ UF4-EJ7/UF4-EJ7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UF4_EJ7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Recorriendo de 1 a 100 con for:");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i+1);
+            }
+        }
+    }
+}

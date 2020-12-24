1d1ea424f705a50f51a1f71afea8892b3a113b0c
Weixin <weixin.xu.2018@gmail.com>
2020-12-24 21:42:08 +0100

    EX13

---
+++ UF4-EJ13/UF4-EJ13/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UF4_EJ13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Intoduce su nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Bienvenido " + nombre);
+        }
+    }
+}

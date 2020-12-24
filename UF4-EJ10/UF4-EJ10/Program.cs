d95d800ba61b1133cf57cb7345aa6832b6483e1d
Weixin <weixin.xu.2018@gmail.com>
2020-12-24 21:40:38 +0100

    EX10

---
+++ UF4-EJ10/UF4-EJ10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UF4_EJ10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mostrando numeros pares o divisibles entre 3 de 1 a 100 con for");
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 2 == 0 || i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}

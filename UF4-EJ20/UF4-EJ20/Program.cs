3f3a0f98011c36526df44877fadd2c92238940ae
Weixin <weixin.xu.2018@gmail.com>
2020-12-24 21:44:47 +0100

    EX20

---
+++ UF4-EJ20/UF4-EJ20/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UF4_EJ20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numero de venta: ");
+            string n = Console.ReadLine();
+            int num = Convert.ToInt32(n);
+
+            int i = 0;
+            double suma = 0;
+
+            while (i < num)
+            {
+                Console.WriteLine("Venta{0}: ", (i+1));
+                string v = Console.ReadLine();
+                int venta = Convert.ToInt32(v);
+                suma = suma + venta;
+                i++;
+            }
+
+            Console.WriteLine("Venta total: " + suma);
+        }
+    }
+}

f9e92987bc5d6902d51e090db451cfb9f472bc62
Weixin <weixin.xu.2018@gmail.com>
2020-12-24 21:45:51 +0100

    EX23

---
+++ CalculadoraInversa/CalculadoraInversa/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CalculadoraInversa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 10, n2 = 3;
+
+            Console.WriteLine("Operaciones disponibles: " +
+                "+ (suma), - (resta), * (multiplica), / (dividir), ^ (exponente), % (modulo)");
+            string signo = Console.ReadLine();
+
+            switch (signo)
+            {
+                case "+":
+                    Console.WriteLine("Suma: " + (n1 + n2));
+                    break;
+                case "-":
+                    Console.WriteLine("Resta: " + (n1 - n2));
+                    break;
+                case "*":
+                    Console.WriteLine("Multiplica: " + (n1 * n2));
+                    break;
+                case "/":
+                    Console.WriteLine("Dividir: " + (n1 / n2));
+                    break;
+                case "^":
+                    Console.WriteLine("Exponente: " + (Math.Pow(n1, n2)));
+                    break;
+                case "%":
+                    Console.WriteLine("Modulo: " + (n1 % n2));
+                    break;
+                default:
+                    Console.WriteLine("Has introducido mal");
+                    break;
+            }
+
+        }
+    }
+}

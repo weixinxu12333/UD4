3937c3714ebcd018bbfc9c8eede1aee6a70b185c
Weixin <weixin.xu.2018@gmail.com>
2020-12-24 21:28:00 +0100

    EX1

---
+++ UF4-EJ1/UF4-EJ1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UF4_EJ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b=2, c=3;
+            int suma = a + b + c;
+            Console.WriteLine("suma="+suma);
+        }
+    }
+}

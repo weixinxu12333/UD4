024633202a4f838e9e8ac566665cf509725d29d5
Weixin <weixin.xu.2018@gmail.com>
2020-12-24 21:41:36 +0100

    EX12

---
+++ UF4-EJ12/UF4-EJ12/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UF4_EJ12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Weixin";
+            Console.WriteLine("Bienvenido " + nombre);
+        }
+    }
+}

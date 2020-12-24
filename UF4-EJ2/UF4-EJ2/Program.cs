d3d7265f309faa8f2e5309273cc493a260672452
Weixin <weixin.xu.2018@gmail.com>
2020-12-24 21:33:41 +0100

    EX2

---
+++ UF4-EJ2/UF4-EJ2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UF4_EJ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe un nombre:");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Escribe una ciudad:");
+            string ciudad = Console.ReadLine();
+
+            Console.WriteLine("Hola {0}, bienvenido a {1}!", nombre, ciudad );
+        }
+    }
+}

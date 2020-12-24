8f54cbab6fd275add111f393ac1a3f10b247d32f
Weixin <weixin.xu.2018@gmail.com>
2020-12-24 21:42:34 +0100

    EX14

---
+++ UF4-EJ14/UF4-EJ14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UF4_EJ14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculo de circulo");
+            Console.WriteLine("Introduce radio:");
+            string r = Console.ReadLine();
+            double radio = Convert.ToDouble(r);
+            const double p = Math.PI;
+            double circulo = p * Math.Pow(radio, 2);
+            Console.WriteLine("Resultado: " + circulo);
+
+        }
+    }
+}
